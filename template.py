001d87d36d704ba871ed8f42ea70283d886799fe
CDE90 <ethan@ethancoward.dev>
2024-12-08 10:37:55 +0000

    Update template to add test

--- template.py
+++ template.py
@@ -10,6 +10,12 @@ def main():
     with open("d{{day}}/input.txt") as f:
         input = f.read()
 
+    test = """
+"""
+
+    print(f"Part 1 test: {p1(test)}")
+    print(f"Part 2 test: {p2(test)}")
+
     print(f"Part 1: {p1(input)}")
     print(f"Part 2: {p2(input)}")
 
